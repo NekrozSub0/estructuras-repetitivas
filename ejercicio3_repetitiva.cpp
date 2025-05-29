d8b80f04a6c36b746aa08e0edd45c77790043ef8
Alexander <alex-c-r@hotmail.es>
2025-05-29 00:35:52 -0500

    Agrega ejercicio 3 de estructuras repetitivas.

---
+++ ejercicio3_repetitiva.cpp
@@ -0,0 +1,16 @@
+//Algoritmo Sumar los N primeros numeros naturales con bucles
+#include <iostream>
+using namespace std;
+int main (){
+	int N,SUM;
+	//Entrada
+	cout<<Ingrese la cantidad de numeros naturales a sumar: <<endl;
+	cin>>N;
+	//Proceso
+	SUM = 0;
+	for (int i = 1; i <= N; i++){
+		SUM = SUM + i;
+	}
+	//Salida
+	cout<<La suma de los <<N<< primeros numeros naturales es :<<SUM<<endl;
+	}
