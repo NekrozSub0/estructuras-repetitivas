5284805d9ed00409bddecb5cf0a5712368277791
Alexander <alex-c-r@hotmail.es>
2025-06-02 11:33:12 -0500

    Agregado de respuesta en caso de ingresarse un numero negativo

--- ejercicio1_repetitiva.cpp
+++ ejercicio1_repetitiva.cpp
@@ -5,9 +5,13 @@ int main (){
 	int n, multiplo;
 	cout<<"Ingrese la cantidad de multiplos de 7 a imprimir"<<endl;
 	cin>>n;
-	for (int i=1;i<=n;i++){
-		multiplo=7*i;
-		cout<<multiplo<<endl;
+	if (n<0){
+		cout<<"ERROR, numero introducido menor a 0"<<endl;
+	} else {
+		for (int i=1;i<=n;i++){
+			multiplo=7*i;
+			cout<<multiplo<<endl;
+		}
 	}
 	return 0;
 }
